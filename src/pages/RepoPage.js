736831578cde1e0ba41f59cec05ca3396cf632ca
Tanakorn Numrubporn <him_aeng@yahoo.co.th>
2017-04-01 14:52:59 +0700

    complete repoPage component

--- src/pages/RepoPage.js
+++ src/pages/RepoPage.js
@@ -10,15 +10,60 @@ const repositories =
   `${host}/search/repositories?q=react-native&sort=stars&order=desc`;
 
 class RepoPage extends Component {
-  render() {
-    return (
-      <View>
-        <Text>
-          Loading..
+  static propTypes = {
+    fetchData: PropTypes.func.isRequired,
+    repos: PropTypes.object.isRequired
+  }
+
+  componentDidMount() {
+    this.props.fetchData(repositories);
+  }
+
+  renderMainPart() {
+    const { isLoading, repos } = this.props.repos;
+    if(!repos || isLoading) {
+      return (
+        <Text style={styles.Loading}>
+          Loading...
         </Text>
+      );
+    }
+
+    return (
+      <RepoList
+        repos={repos}
+      />
+    );
+  }
+
+  render() {
+    const { isLoading, repos } = this.props.repos;
+    return (
+      <View style={styles.container}>
+        {this.renderMainPart()}
       </View>
     );
   }
 }
 
-export default RepoPage;
+const styles = {
+  container: {
+    flex: 2,
+    justifyContent: 'center'
+  },
+  loading: {
+    fontSize: 20,
+    alignSelf: 'center'
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    repos: state.repos
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  { fetchData }
+)(RepoPage);
