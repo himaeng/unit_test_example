111cfa38e76d29de5b28383a733076e4e58dca93
Tanakorn Numrubporn <him_aeng@yahoo.co.th>
2017-04-01 14:40:58 +0700

    add url for api call

--- src/pages/RepoPage.js
+++ src/pages/RepoPage.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { View, Text } from 'react-native';
+
+import RepoList from '../components/RepoList';
+import { fetchData } from '../actions';
+
+const host = 'https://api.github.com';
+const repositories =
+  `${host}/search/repositories?q=react-native&sort=stars&order=desc`;
 
 class RepoPage extends Component {
   render() {
